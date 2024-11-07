8245a6f4d2566e0db688c6dab6907455ddfa7fb0
jenilparmar <jerryhash231@gmail.com>
2024-11-07 19:09:56 +0530

    Animation Corrected

--- Jenil/Components/SecondHeader.jsx
+++ Jenil/Components/SecondHeader.jsx
@@ -24,11 +24,11 @@ const SecondHeader = () => {
   return (
     <div className="w-full md:w-full mx-4 md:mx-20 pt-8 pb-2 md:my-10 md:h-[25em] border-l-[1px] md:border-l-2 border-b-[1px] md:border-b-2 border-yellow-300 flex flex-col items-center md:items-start">
       <p className="ready text-5xl sm:text-6xl md:text-[8em] lg:text-[10em] text-[#003c5f] font-bold md:pl-16 md:pt-16">
-        Ready?
+        INSPIRED
       </p>
       <div className="secondLine text-base sm:text-lg md:text-[2em] text-[#003c5f] mt-3 md:mt-8 flex flex-row items-center md:pl-20">
-        <span>TO THE JOURNEY OF BEING</span>
-        <span className="font-bold px-1 md:px-2">AWESOME!!</span>
+        <span>TO TRANSFORM IDEAS INTO</span>
+        <span className="font-bold px-1 md:px-2">REALITY!</span>
       </div>
       <div className="w-10/12 flex flex-col md:flex-row gap-1 pr-4 md:gap-2 md:mt-6 md:ml-16 mt-2 md:px-2 ">
         <div className="flex flex-row   ">
