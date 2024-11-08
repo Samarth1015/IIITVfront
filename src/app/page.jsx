9e755379b31beb4ae64b64933d0f70e63c93ead3
jenilparmar <jerryhash231@gmail.com>
2024-11-08 11:24:11 +0530

    .

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,7 +5,7 @@ import Initialheader from "../../Jenil/Components/Initialheader";
 import SecondHeader from "../../Jenil/Components/SecondHeader";
 import MainContent1 from "../../Jenil/Components/MainContent1";
 import dynamic from "next/dynamic";
-import Animate from "../../Samarth/Components/Animate";
+
 import Footer from "../../Samarth/Components/Footer";
 
 const ExtraPage = dynamic(() => import("../../Jenil/Components/ExtraPage"), {
@@ -18,7 +18,7 @@ export default function Home() {
       <div className="flex flex-col overflow-x-hidden overflow-y-hidden">
         <Initialheader />
         <SecondHeader />
-        {/* <Animate /> */}
+
         <MainContent1 />
         <ExtraPage />
         <Footer />
