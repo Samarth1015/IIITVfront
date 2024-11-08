7d1250141adc03e655d2ff08890512e531924791
jenilparmar <jerryhash231@gmail.com>
2024-11-08 09:52:14 +0530

    Correcteed order of page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,6 +5,7 @@ import Initialheader from "../../Jenil/Components/Initialheader";
 import SecondHeader from "../../Jenil/Components/SecondHeader";
 import MainContent1 from "../../Jenil/Components/MainContent1";
 import dynamic from "next/dynamic";
+import Animate from "../../Samarth/Components/Animate";
 
 const ExtraPage = dynamic(() => import("../../Jenil/Components/ExtraPage"), {
   ssr: false,
@@ -17,6 +18,7 @@ export default function Home() {
       <div className="flex flex-col overflow-x-hidden overflow-y-hidden">
         <Initialheader />
         <SecondHeader />
+        <Animate/>
         <MainContent1 />
         <ExtraPage />
       </div>
